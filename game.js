a6762c1c6712ae2577b5b906d8e9a50c2fbc63e7
Brandon Brumfield <brandonbrumfield7@gmail.com>
2025-05-28 16:52:00 -0500

    Switch game logic to DOM manipulation and interactive UI

--- game.js
+++ game.js
@@ -1,33 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function playGame() {
-
-    while (humanScore < 5 && computerScore < 5) {
-        playRound(getHumanChoice(), Math.random());
-    }
-
-    if (humanScore == 5) {
-        console.log("The player wins!")
-    }
-
-    else console.log("The computer wins!")
-}
-
-
-
-
-
-
-function getHumanChoice() {
-    let humanChoice = prompt("Please enter rock, paper, or scissors.");
-    return humanChoice
-
-}
 
 function playRound(humanChoice, computerChoice) {
     let computerConverted;
-    let humanConverted = humanChoice.toLowerCase()
+    let humanConverted = humanChoice;
     
     if (computerChoice < 0.33) {
         computerConverted = "Rock"
@@ -46,40 +23,98 @@ function playRound(humanChoice, computerChoice) {
         (computerConverted == "Paper" && humanConverted == "paper") ||
         (computerConverted == "Rock" && humanConverted == "rock")
     ) {
-        console.log("A tie!")
+        round_result.textContent = ("A tie!")
     }
 
     if (computerConverted == "Rock" && humanConverted == "scissors") {
-        console.log("Rock beats scissors, the computer wins the round!")
+        round_result.textContent = ("Rock beats scissors, the computer wins the round!")
         computerScore++
     }
 
     if (computerConverted == "Rock" && humanConverted == "paper") {
-        console.log("Paper beats rock, the player wins the round!")
+        round_result.textContent = ("Paper beats rock, the player wins the round!")
         humanScore++
     }
 
     if (computerConverted == "Paper" && humanConverted == "scissors") {
-        console.log("Scissors beats paper, the player wins the round!")
+        round_result.textContent = ("Scissors beats paper, the player wins the round!")
         humanScore++
     }
 
     if (computerConverted == "Paper" && humanConverted == "rock") {
-        console.log("Paper beats rock, the computer wins the round!")
+        round_result.textContent = ("Paper beats rock, the computer wins the round!")
         computerScore++
     }
 
     if (computerConverted == "Scissors" && humanConverted == "paper") {
-        console.log("Scissors beats paper, the computer wins the round!")
+        round_result.textContent = ("Scissors beats paper, the computer wins the round!")
         computerScore++
     }
 
     if (computerConverted == "Scissors" && humanConverted == "rock") {
-        console.log("Rock beats scissors, the player wins the round!")
+        round_result.textContent = ("Rock beats scissors, the player wins the round!")
         humanScore++
+    }
+
+    score_container.appendChild(round_result);
+
+    human_score.textContent = (`Human score is: ${humanScore}`);
+    score_container.appendChild(human_score);
+
+    computer_score.textContent = (`Computer score is: ${computerScore}`);
+    score_container.appendChild(computer_score);
+
+    if (humanScore == 5) {
+        human_score.textContent = "";
+        computer_score.textContent = "";
+
+        round_result.textContent = "The human player wins! Click any of the three options to start another round."
+
+        humanScore = 0;
+        computerScore = 0;
+    }
+
+    if (computerScore == 5) {
+        human_score.textContent = "";
+        computer_score.textContent = "";
+
+        round_result.textContent = "The computer player wins! Click any of the three options to start another round."
+
+        humanScore = 0;
+        computerScore = 0;
     }
 
 
 
+
+
 }
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+let score_container = document.querySelector("#score_container");
+const human_score = document.createElement("p");
+const computer_score = document.createElement("p");
+const round_result = document.createElement("p");
+
+round_result.textContent = "Please select an option. First to 5 wins."
+score_container.appendChild(round_result);
+
+rock.addEventListener("click", () => {
+        playRound("rock", Math.random());
+
+})
+
+paper.addEventListener("click", () => {
+        playRound("paper", Math.random());
+  
+})
+
+scissors.addEventListener("click", () => {
+        playRound("scissors", Math.random());
+
+            
+})
 
